199b721b06cb7d140fe98472e298506b35f951cc
Andrew Hoffmann <ashoffmann90@gmail.com>
2020-06-17 17:15:17 -0700

    MVP ish

--- product_of_all_other_numbers/product_of_all_other_numbers.py
+++ product_of_all_other_numbers/product_of_all_other_numbers.py
@@ -1,16 +1,34 @@
 '''
 Input: a List of integers
 Returns: a List of integers
+Write a function that receives an array of integers and returns an array consisting of the product of all numbers in the array _except_ the number at that index. 
 '''
+
+
 def product_of_all_other_numbers(arr):
     # Your code here
+    # m = []
+    # r = 1
+    #     for i in arr:
+    #         for x in arr:
 
-    pass
+    r = 1
+    for i in arr:
+        r *= i
+    return [r / n for n in arr]
+
+    # find the element at each index
+    # multiply every element in array minus the current index
+    # point to current_index
+    # increment index each time
+    # remove that index
 
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     # arr = [1, 2, 3, 4, 5]
-    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8, 9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
+    arr = [2, 6, 9, 8, 2, 2, 9, 10, 7, 4, 7, 1, 9, 5, 9, 1, 8, 1, 8, 6, 2, 6, 4, 8,
+           9, 5, 4, 9, 10, 3, 9, 1, 9, 2, 6, 8, 5, 5, 4, 7, 7, 5, 8, 1, 6, 5, 1, 7, 7, 8]
 
-    print(f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
+    print(
+        f"Output of product_of_all_other_numbers: {product_of_all_other_numbers(arr)}")
--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -1,16 +1,30 @@
 '''
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
+Given an array of integers, there is a sliding window of size `k` which is moving from the left side of the array to the right, one element at a time. You can only interact with the `k` numbers in the window. Return an array consisting of the maximum value of each window of elements.
 '''
+
+
 def sliding_window_max(nums, k):
     # Your code here
+    # init empty arr and append max vals to each window
+    max_vals = []
+    # loop through nums arr from k to len(nums) +1
+    for num in range(k, len(nums) + 1):
+        # create window from num - k to num
+        window = nums[num - k:num]
+        max_vals.append(max(window))
+    # return max vals
+    return max_vals
 
-    pass
+    # one liner
+    # [max(nums[num - k: num]) for num in range(k, len(nums) + 1)]
 
 
 if __name__ == '__main__':
-    # Use the main function here to test out your implementation 
+    # Use the main function here to test out your implementation
     arr = [1, 3, -1, -3, 5, 3, 6, 7]
     k = 3
 
-    print(f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
+    print(
+        f"Output of sliding_window_max function is: {sliding_window_max(arr, k)}")
